b2b20193a875c59c3cff0a713f03720acbc5fbd3
Daniil Khrapov <Tyuiu.KhrapovDY@outlook.com>
2024-11-10 14:25:49 +0500

    Task3

---
+++ Tyuiu.KhrapovDY.Sprint4.Task3.V2.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+using System.Reflection.Metadata.Ecma335;
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.KhrapovDY.Sprint4.Task3.V2.Lib
+{
+    public class DataService : ISprint4Task3V2
+    {
+        public int Calculate(int[,] array)
+        {
+            int rows = array.GetUpperBound(0) + 1;
+            int columns = array.Length / rows;
+
+            int max = 0;
+
+            for (int i = 0; i < rows; i++) 
+            {
+                for (int j = 0; j < columns; j++) 
+                {
+                    if (array[i, j] > max)
+                    {
+                        max = array[i, j];
+                    }
+                }
+            }
+            return max;
+        }
+       
+    }
+   
+}
---
+++ Tyuiu.KhrapovDY.Sprint4.Task3.V2/Program.cs
@@ -0,0 +1,55 @@
+﻿using Tyuiu.KhrapovDY.Sprint4.Task3.V2.Lib;
+
+namespace Tyuiu.KhrapovDY.Sprint4.Task3.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mtrx = new int[5, 5] { { 4, 3, 2, 8, 7 }, { 4, 6, 5, 5, 7 }, { 3, 8, 8, 3, 5 }, { 5, 7, 5, 4, 4 }, { 8, 8, 3, 7, 6 } };
+
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int columns = mtrx.Length / rows;
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Храпов Д. Ю. | СМАРТб-24-1";
+
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* Спринт #4                                                                         *");
+            Console.WriteLine("* Тема: Одномерные массивы (статический ввод)                                       *");
+            Console.WriteLine("* Задание #3                                                                        *");
+            Console.WriteLine("* Вариант #2                                                                        *");
+            Console.WriteLine("* Выполнил: Храпов Даниил Юрьевич | СМАРТб-24-1                                     *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                          *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный статическими     *");
+            Console.WriteLine("* значениями в диапазоне от 2 до 8. Найдите максимальный элемент в первой строке    *");
+            Console.WriteLine("* массива.                                                                          *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                  *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.WriteLine($"{mtrx[i, j]} \t");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                        *");
+            Console.WriteLine("*************************************************************************************");
+            int res = ds.Calculate(mtrx);
+
+            Console.WriteLine("Максимальный элемент = " + res);
+            Console.ReadKey();
+
+
+        }
+    }
+}
